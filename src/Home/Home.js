ddb817376ad2085d0c7c9ad507338efd112cb2b6
DimaDK <DimaKrutDK@ya.ru>
2018-09-12 19:11:25 +0300

    added all models from website and simplied loadModification logic

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -92,7 +92,27 @@ class Home extends React.Component {
 
     ctaText = `Отправьте заявку, наши сотрудники свяжутся с вами и запишут на техническое обслуживание автомобиля Nissan`
 
-    models = [{id: 48, value: 'Almera Classic (2006-2013)'}]
+    models = [
+        {id: 43, value: 'Almera Classic (2006-2013)'},
+        {id: 44, value: 'Almera New (с 2012)'},
+        {id: 45, value: 'Juke (с 2011)'},
+        {id: 46, value: 'Micra (с 2002)'},
+        {id: 47, value: 'Murano (2008-2015)'},
+        {id: 48, value: 'Murano (с 2015)'},
+        {id: 49, value: 'Navara (с 2010)'},
+        {id: 50, value: 'Note (2006-2014)'},
+        {id: 51, value: 'Pathfinder (2010-2013)'},
+        {id: 52, value: 'Pathfinder (с 2013)'},
+        {id: 53, value: 'Qashqai (2006-2013)'},
+        {id: 54, value: 'Qashqai (с 2013)'},
+        {id: 55, value: 'Teana (2008-2013)'},
+        {id: 56, value: 'Teana (с 2013)'},
+        {id: 57, value: 'Terrano (с 2013)'},
+        {id: 58, value: 'Tiida (2004-2015)'},
+        {id: 60, value: 'X-Trail (2010-2014)'},
+        {id: 61, value: 'X-Trail (с 2014)'},
+        {id: 62, value: 'Sentra (с 2014)'}
+    ]
 
     modifications = [{id: 1, value: 'лучшая'}]
 
@@ -411,19 +431,10 @@ class Home extends React.Component {
         })
     }
 
-    loadModifications(modelId) {
-        return new Promise(async (resolve, reject) => {
-           try {
-                const response = await axios.post(
-                    `https://dimadk.tk/proxy.php?miniProxyFormAction=https://ya-service-nissan.ru/ajax/model.php&ID=${modelId}`,
-                )
-                console.log(response);
-                resolve(response.data)
-           } catch (err) {
-               console.log(err)
-               reject(err)
-           }
-        })
+    async loadModifications(modelId) {
+        return (await axios.post(
+            `https://dimadk.tk/proxy.php?miniProxyFormAction=https://ya-service-nissan.ru/ajax/model.php&ID=${modelId}`
+        )).data
     }
 
     getItemId(item) {
