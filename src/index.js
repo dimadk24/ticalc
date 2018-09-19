a88e79fe4d1e266e799f6850a6a6d74071d47b42
DimaDK <DimaKrutDK@ya.ru>
2018-09-19 16:39:10 +0300

    prettied

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import * as VKConnect from '@vkontakte/vkui-connect';
-import App from './App';
-import registerServiceWorker from './sw';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
+import * as VKConnect from '@vkontakte/vkui-connect'
+import App from './App'
+import registerServiceWorker from './sw'
 import Raven from 'raven-js'
 
-const root = document.getElementById('root');
-Raven.config('https://e1f809f399e2427898e1796a4a4d8c64@sentry.io/1280607').install()
+Raven.config(
+    'https://e1f809f399e2427898e1796a4a4d8c64@sentry.io/1280607'
+).install()
+
+const root = document.getElementById('root')
 // Render
 const render = (Component) => {
-	ReactDOM.render(
-		<AppContainer>
-			<Component />
-		</AppContainer>,
-		root,
-	);
-};
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        root
+    )
+}
 
 // Init VK App
-VKConnect.send('VKWebAppInit', {});
+VKConnect.send('VKWebAppInit', {})
 
-render(App);
+render(App)
 
 // Service Worker For Cache
-registerServiceWorker();
+registerServiceWorker()
 
 // Hot Reload
 if (module.hot) {
-	module.hot.accept('./App', () => { render(App); });
+    module.hot.accept('./App', () => {
+        render(App)
+    })
 }
