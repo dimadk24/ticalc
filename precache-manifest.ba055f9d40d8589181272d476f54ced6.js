11aa0e73428f2ff47479d7be22c7f98b94373593
DimaDK <DimaKrutDK@ya.ru>
2019-03-18 20:37:26 +0300

    Updates

---
+++ precache-manifest.ba055f9d40d8589181272d476f54ced6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d10b61e4acc8e847a6579e94fa07223",
+    "url": "/ticalc/static/media/logo.4d10b61e.png"
+  },
+  {
+    "revision": "9d64e402553e75f6293d",
+    "url": "/ticalc/static/js/runtime~main.9d64e402.js"
+  },
+  {
+    "revision": "83ca495765986e3be24c",
+    "url": "/ticalc/static/js/main.83ca4957.chunk.js"
+  },
+  {
+    "revision": "bddc45c13eccbbdde566",
+    "url": "/ticalc/static/js/2.bddc45c1.chunk.js"
+  },
+  {
+    "revision": "83ca495765986e3be24c",
+    "url": "/ticalc/static/css/main.e0d5ca30.chunk.css"
+  },
+  {
+    "revision": "bddc45c13eccbbdde566",
+    "url": "/ticalc/static/css/2.91b7b8c3.chunk.css"
+  },
+  {
+    "revision": "1708b9fb86272ebf59f9ba97bc027607",
+    "url": "/ticalc/index.html"
+  }
+];
